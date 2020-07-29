00f444835db2af26e14efd9b4351d36661986d85
admin <admin@admins-MacBook-Air.local>
2020-07-29 14:08:41 +0100

    add comments

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,7 @@
 // import People from "./usersModule";
 // console.log(People);
 window.onload = ()=>{
-	
+	// grab age ,gender,height,imgsrc and card-wrapper
 const img = document.querySelector('#imgsrc');
 const age = document.querySelector('#age');
 const gender = document.querySelector('#gender');
@@ -9,7 +9,6 @@ const height = document.querySelector('#height');
 const cardWrapper = document.querySelector('#card-wrapper');
 
 // define users class
-
 class Users{
 	constructor(user){
 		this.name = user.name;
@@ -18,7 +17,7 @@ class Users{
 		this.height = user.height;
 		this.url = user.url;
 	}
-
+// create user method
 	user(name,birth_year,gender,height){
 	return	{
 	     "name" :this.name,
@@ -35,19 +34,23 @@ class Users{
 fetch('https://swapi.dev/api/people/')
 .then(res=>res.json())
 .then(({results})=>{
+	// store results in users
   let users = results;
+//   populate card for each user
   for(user of users){
 	  let star = new Users(user);
-
+	//   get user() method
 	  let starDetails = star.user();
+	//   create a container for user img,name and extra details
 	let listItemContainer = document.createElement('div');
+	// img tag
 	let listImg = document.createElement('img');
-	
+	// create h3 to store name
 	let listName = document.createElement('h3');
-	
+	// append name to listname
 	listName.appendChild(document.createTextNode(starDetails.name));
 
-
+// set src attribute of img
 	if(starDetails.gender === 'female'){
 		listImg.setAttribute('src','assets/img/girl.svg');
 	}
@@ -55,31 +58,42 @@ fetch('https://swapi.dev/api/people/')
 		listImg.setAttribute('src','assets/img/man.svg');
 	}
 
+	// set id for img
 	listImg.setAttribute('id','listImgId');
+	// append listimg that is img to listitemcontainer
 	listItemContainer.appendChild(listImg);
+	// append name
 	listItemContainer.appendChild(listName);
+	// set id 
 	listItemContainer.setAttribute('id','listItemContainer');
-	// listItemContainer.appendChild(document.querySelector('#details'));
-    let ul = document.getElementById("list");
+	
+	// grab #list
+	let ul = document.getElementById("list");
+	
+	// create a div
 	let div = document.createElement("div");
 	
+	// set id for div
 	div.setAttribute('id','main-card');
+	// append listItemcontainer to div
     div.appendChild(listItemContainer);
 	
-    div.setAttribute('class', 'list-item');
+	// set class 
+	div.setAttribute('class', 'list-item');
+	// append div to ul
     ul.appendChild(div);  
 	
     // set click event
     div.addEventListener("click",(e)=>{
 	
-
+//  set textcontent of gender
 		gender.textContent = `Gender: ${starDetails.gender}`;
-
+//  set textcontent of height
 		height.textContent = `Height: ${starDetails.height} meters`;
-
+//  set textcontent of age
 		age.textContent = `Birth year: ${starDetails.birth_year}`;
 		
-		
+		//  append #details to listitemcoontainer
 			listItemContainer.appendChild(document.querySelector('#details'));
 
 })
